79284f5924e709674e66450299394397f43cb2e1
annetnyabuto <anngnyabuto@gmail.com>
2024-03-12 19:26:46 +0300

    This is code that print a personalised message

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age = input(" what is your actual age?")
+location = ("where do you live?")
+#print personalised message
+print (" Hello " + name + ", you are " + age + "years old and live in " + location + ".")
